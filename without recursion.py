149e9986f5db27e8adcdff63c794ff126f49ac83
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2025-03-20 09:14:01 +0700

    Update without recursion.py

--- without recursion.py
+++ without recursion.py
@@ -3,9 +3,20 @@ def recursivecaesarcipher(text, shift):
         return ""
     
     plaintext = ""
-  
-    for i, j in enumerate(text): #index, char
-        if j == " ":
-            continue
-        plaintext = plaintext + chr(ord(j)+shift)
+
+    for char in text:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            
+            shiftedchar = ord(char) + shift
+            if shiftedchar > ord('z'):
+                
+
+
+            plaintext += shiftedchar
+
     return plaintext
+
+
+
+print(recursivecaesarcipher("abc", 3))
